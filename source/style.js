12b4ae130a22777e011de28e5fb6b929bbc35261
Aditya Mahesh & Spencer Smitherman <pair+amahesh0501.bigspencey@devbootcamp.com>
2014-02-03 15:29:56 -0800

    Completed Release 0 with help

--- source/style.js
+++ source/style.js
@@ -1,4 +1,12 @@
-// Shorthand for document ready
-$(function(){
-  // your code goes here.
-});
+$(function (){
+  var adjustCss = function() {
+  var selector = $("#style_editor > input[name=selector]").val()
+  var property = $("#style_editor > input[name=property]").val()
+  var value = $("#style_editor > input[name=value]").val()
+  $(selector).css(property, value)
+}
+
+  $('#style_editor > input[type=submit]').on('click', function(e) {
+    e.preventDefault();
+    adjustCss();
+  })
