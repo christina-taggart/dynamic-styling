731589fdebde08525783ae00e4fea4ebb491ee40
Alexander Leishman & John Olmsted <pair+leishman.qsymmachus@devbootcamp.com>
2014-02-03 12:50:08 -0800

    Successfully captured form input with jQuery

--- source/style.js
+++ source/style.js
@@ -1,4 +1,6 @@
 // Shorthand for document ready
 $(function(){
-  // your code goes here.
+  var selector = $('#style_editor > input[name=selector]').val()
+  var property = $('#style_editor > input[name=property]').val()
+  var value = $('#style_editor > input[name=value]').val()
 });
