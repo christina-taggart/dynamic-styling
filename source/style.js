5165f2f1e0c507b6dabf3f1583b73d1e940b3d7a
Ricardo Rubio <rickarubio@gmail.com>
2014-02-03 18:10:06 -0800

    added js to control css changes from form submission

--- source/style.js
+++ source/style.js
@@ -1,4 +1,12 @@
 // Shorthand for document ready
-$(function(){
-  // your code goes here.
+$(function() {
+var cssPropertyChanger = function(event) {
+  event.preventDefault();
+  var selector = $("input[name='selector']").val();
+  var property = $("input[name='property']").val();
+  var value = $("input[name='value']").val();
+  $(selector).css(property, value);
+};
+form = $('#style_editor');
+form.on("submit", cssPropertyChanger);
 });
