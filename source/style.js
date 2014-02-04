c5fad926a0ccd38c84c9d2f39676886d8cef76b5
Darcey Lachtman <dslachtman@gmail.com>
2014-02-03 18:36:46 -0800

    all releases

--- source/style.js
+++ source/style.js
@@ -1,4 +1,21 @@
 // Shorthand for document ready
 $(function(){
   // your code goes here.
+  console.log("ready")
+  $('#style_editor').submit(function(event){
+  		
+  		var selector = $("input[name=selector]").val();
+  		console.log(selector)
+  		var property =  $("input[name=property]").val();
+  		var value = $("input[name=value]").val();
+  		$(selector).css(property, value);
+  		event.preventDefault();
+  	})
 });
+
+
+
+//<input name="selector" placeholder="css selector" />
+        // <input name="property" placeholder="property" />
+        // <input name="value" placeholder="value" />
+        // <input type="submit" value="Style it!" />
