e3cba9f040b0c227aa4ac79a41e660df90885b68
& <pair+EmFoley.epchalfant@devbootcamp.com>
2014-02-03 17:20:41 -0800

    used event handlers to change the DOM based on user input

--- source/style.js
+++ source/style.js
@@ -1,4 +1,20 @@
 // Shorthand for document ready
+// $(function(){
+//   // your code goes here.
+//   on click that style thing!! do some shit !!!!!
+// });
+
+
 $(function(){
-  // your code goes here.
-});
+    var adjustCSS = function () {
+    var selector = $(".selector").val();
+    var property = $(".property").val();
+    var value = $(".value").val();
+    $(selector).css(property,value);
+  }
+$("#style_editor > input[type=submit]").on( "click",  function(event) {
+  event.preventDefault();
+  adjustCSS();
+})
+
+})
