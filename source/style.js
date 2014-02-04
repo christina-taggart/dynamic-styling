0a6af97515ccf51c95a3c18754c2c9d9cf87f6cd
& <pair+lee-sh-roy.mh120888@devbootcamp.com>
2014-02-03 17:09:01 -0800

    User can change CSS in page

--- source/style.js
+++ source/style.js
@@ -1,4 +1,11 @@
 // Shorthand for document ready
 $(function(){
-  // your code goes here.
+  $("#style_editor").on("submit", function(event) {
+    event.preventDefault();
+    var form = document.querySelector('#style_editor');
+    var userSelector = form["selector"].value;
+    var userProperty = form["property"].value;
+    var userValue = form["value"].value;
+    $(userSelector).css(userProperty, userValue);
+  });
 });
