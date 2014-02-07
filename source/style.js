c61d1f616a1159e6d6605687eacc6119850b6ea5
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-06 16:04:12 -0800

    Added form functionality

--- source/style.js
+++ source/style.js
@@ -1,4 +1,11 @@
 // Shorthand for document ready
 $(function(){
-  // your code goes here.
+  $("#style_editor").on("submit", function(e){
+    e.preventDefault();
+    var selector = $("input[name='selector']").val();
+    var property = $("input[name='property']").val();
+    var value = $("input[name='value']").val();
+    $(selector).css(property, value)
+  });
 });
+
